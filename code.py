a58a55ed28bf4d811066dc9da7cb3ab0c0cf3455
OleksandrVahin <50240069+OleksandrVahin@users.noreply.github.com>
2022-04-28 12:36:35 +0300

    Add matrix-transformation function and matrix-print function

--- code.py
+++ code.py
@@ -3,6 +3,8 @@ A = ((4.855, 1.239, 0.272, 0.258),
      (0.456, 0.285, 4.354, 0.254),
      (0.412, 0.335, 0.158, 2.874))
 
+B1 = (1.192, 0.256, 0.852, 0.862)
+
 
 def check_diagonal_advantage(matrix):
     """Takes square matrix, returns True if matrix has a diagonal advantage, else False"""
@@ -16,4 +18,27 @@ def check_diagonal_advantage(matrix):
     return all(check_line)
 
 
+def highlight_root(A, B):
+    """Takes two matrices(A, B from Ax = B) and returns two matrices(B, C from x = Bx + C)"""
+    new_B = []
+    C = []
+    for i in range(len(A)):
+        new_B.append(tuple(round(-n / A[i][i], 6) for n in A[i][:i] + A[i][i + 1:]))
+        C.append((round(B[i] / A[i][i], 6), ))
+    return tuple(new_B), tuple(C)
+
+
+def print_matrix(matrix):
+    """Takes matrix and print it to the stdout"""
+    for line in matrix:
+        for num in line:
+            print(str(num).ljust(10), end=' ')
+        print()
+
+
+
+
 print(check_diagonal_advantage(A))
+B2, C = highlight_root(A, B1)
+print_matrix(B2)
+print_matrix(C)
