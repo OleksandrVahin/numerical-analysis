54f8c42161816e3b2333fc5cf2fa865f381d9498
OleksandrVahin <50240069+OleksandrVahin@users.noreply.github.com>
2022-05-25 21:42:02 +0300

    Add newton search

--- NSE_solver.py
+++ NSE_solver.py
@@ -81,7 +81,30 @@ def second_system(vector):
     return np.tan(vector[0] * vector[1] + 0.3) - vector[0] ** 2, 0.7 * (vector[0] ** 2) + 2 * (vector[1] ** 2) - 1
 
 
+def jacobian(vector):
+    """Returns jacoby matrix of the second system in the point"""
+    x, y = vector
+    m00 = y / np.cos(x * y + 0.3) ** 2 - 2 * x
+    m01 = x / np.cos(x * y + 0.3) ** 2
+    m10 = 1.8 * x
+    m11 = 4 * y
+    return (m00, m01), (m10, m11)
+
+
+def newton_search(x_0, eps, jacoby, equations):
+    log = [(x_0, 0)]
+    while True:
+        difference = tuple(np.linalg.solve(jacoby(x_0), tuple(-x for x in equations(x_0))))
+        x_1 = tuple(x + y for x, y in zip(difference, x_0))
+        delta = norm_max(vector_difference(x_1, x_0))
+        log.append((x_1, delta))
+        if delta <= eps:
+            return x_1, log
+        x_0 = x_1
+
+
 # First task
+print("First task\n")
 
 # Looking graphically for first initial approximation
 print_intersection(x1, y1)
@@ -100,4 +123,19 @@ for j in range(5):
 
 # Calculating solution via SciPy
 print("\nVia SciPy")
-print("Root is", fsolve((lambda x: (np.cos(x[1] - 2) + x[0], np.sin(x[0] + 0.5) - x[1] - 1)), np.array(0.5, -0.1)))
+print("Root is", fsolve((lambda x: (np.cos(x[1] - 2) + x[0], np.sin(x[0] + 0.5) - x[1] - 1)), (0.5, -0.1)))
+
+# Second task
+
+print("\nSecond task\n")
+initial_approximations = ((-0.9, -0.4), (-0.3, 0.7), (0.3, -0.7), (0.9, 0.4))
+
+# Calculating solution
+for j in range(len(initial_approximations)):
+    print(f"Root №{j + 1}")
+    solution, history = newton_search(initial_approximations[j], 0.00001, jacobian, second_system)
+    print_log(history)
+    print("Solution of system is:", tuple(map(r5, solution)))
+    print(second_system(solution))
+
+
