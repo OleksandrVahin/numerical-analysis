67026ffa784cce5c4af86cfa90ab28b1b03c848f
OleksandrVahin <50240069+OleksandrVahin@users.noreply.github.com>
2022-05-25 11:11:05 +0300

    Add simple iterations method

--- NSE_solver.py
+++ NSE_solver.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from functools import partial
 
 
 # First task initial data
@@ -22,6 +23,9 @@ def print_intersection(f1, f2):
     plt.show()
 
 
+r5 = partial(round, ndigits=5)
+
+
 def norm_max(vector):
     """Takes vector, returns its Chebyshev's norm"""
     return max(map(abs, vector))
@@ -32,5 +36,33 @@ def vector_difference(a, b):
     return tuple(t - s for t, s in zip(a, b))
 
 
+def simple_iterations(x_0, eps, equations):
+    """
+    Solves system of nonlinear equations
+    Takes:
+        x_0 - initial approximation
+        eps - precision
+        equations - equations of system
+    Returns:
+        x_1 - vector of approximate solutions
+        log - history of iterations
+    """
+    equations = equations[::-1]
+    log = [(x_0, 0)]
+    while True:
+        x_1 = tuple(equations[i](x_0[i]) for i in range(len(x_0)))[::-1]
+        delta = norm_max(vector_difference(x_1, x_0))
+        log.append((x_1, delta))
+        if delta <= eps:
+            return x_1, log
+        x_0 = x_1
+
+
+# First task
+
 # Looking graphically for first initial approximation
 print_intersection(x1, y1)
+
+# Calculating solution
+solution, history = simple_iterations((0.5, -0.1), 0.00001, (x1, y1))
+print("Solution of system is:", tuple(map(r5, solution)))
