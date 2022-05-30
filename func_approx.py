ac93b6884b36f6915044d293401ae038084d40a3
OleksandrVahin <50240069+OleksandrVahin@users.noreply.github.com>
2022-05-30 11:31:08 +0300

    Add first task function

--- func_approx.py
+++ func_approx.py
@@ -7,3 +7,8 @@ def chebyshev_nodes(a, b, n):
     for i in range(n + 1):
         nodes.append((b + a) / 2 - (b - a) / 2 * np.cos((2 * i + 1) / (2 * n + 2) * np.pi))
     return nodes
+
+
+def func1(x):
+    """Returns value of function from first task"""
+    return 2 * x - x ** 2 - 2 * np.cos(x - 1)
