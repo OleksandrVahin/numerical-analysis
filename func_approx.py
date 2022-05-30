b55f244f55d527f7159312a94f833f037cc44394
OleksandrVahin <50240069+OleksandrVahin@users.noreply.github.com>
2022-05-30 12:28:19 +0300

    Add finite differences function

--- func_approx.py
+++ func_approx.py
@@ -12,3 +12,28 @@ def chebyshev_nodes(a, b, n):
 def func1(x):
     """Returns value of function from first task"""
     return 2 * x - x ** 2 - 2 * np.cos(x - 1)
+
+
+def finite_differences(func_values):
+    """Takes values of function in nodes, returns list of finite differences all orders"""
+    n = len(func_values)
+    diffs = [func_values]
+    while n > 1:
+        diffs.append(tuple(diffs[-1][i + 1] - diffs[-1][i] for i in range(n - 1)))
+        n -= 1
+    return diffs
+
+
+# First Task
+
+# Calculating values in nodes
+nodes = (-2, 0, 2, 4)
+values = tuple(func1(node) for node in nodes)
+print("Function value in nodes:")
+for node in values:
+    print('{:.5f}'.format(node), end=' ')
+
+# Calculating finite differences
+differences = finite_differences(values)
+print("\n\nFinite differences:")
+print(*differences, sep='\n')
